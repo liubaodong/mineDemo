3870f7cdf04f1ba71282fb41fe351dbfb3aa2880
sir-lbd <2458825436@qq.com>
2018-12-07 09:15:18 +0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import {  AfterViewInit,
+  Component,
+  OnInit } from '@angular/core';
+import Swiper from 'swiper';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements AfterViewInit{
+  testSwiper: Swiper;
+  slides = [
+    'https://via.placeholder.com/300x200/FF5733/ffffff',
+    'https://via.placeholder.com/300x200/C70039/ffffff',
+    'https://via.placeholder.com/300x200/900C3F/ffffff'
+  ];
+  title = 'Demo';
+  ngAfterViewInit() {
+    this.testSwiper = new Swiper('.swiper-container', {
+      direction: 'horizontal',
+      loop: true,
+      // 如果需要分页器
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      // 如果需要前进后退按钮
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      // 如果需要滚动条
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+  }
+}
